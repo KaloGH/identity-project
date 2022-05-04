b7ac815d3d1190edea1baaf2ee6bcf9b3bd253c5
Kaloyan Emilov Kirov <kaloalku@gmail.com>
2022-05-04 04:23:04 +0100

    Finished Login design. Only put functionality to buttons left

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/foundation.dart';
+import 'package:identity_project/utils/colors.dart';
+import 'package:identity_project/widgets/text_input_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,24 +12,123 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Creamos controllers para pasar a los inputs
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Dependiendo si es escritorio o móvil, cargamos un logo u otro
+    final StatefulWidget logo;
+    if (kIsWeb) {
+      logo = SvgPicture.asset(
+        'svg/logo_white.svg',
+        height: 350,
+      );
+    } else {
+      logo = Image.asset(
+        'assets/images/logo_white.png',
+        height: 350,
+      );
+    }
+
     return Scaffold(
-        body: SafeArea(
-            child: Container(
-      padding: const EdgeInsets.symmetric(horizontal: 32),
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          //svg logo
-          //Inputs of email and password
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 1),
+              //svg logo
+              logo,
+              const SizedBox(
+                height: 34,
+              ),
+              //Inputs of email and password
+              TextInputField(
+                controller: _emailController,
+                hint: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              // MARGIN BETWEEN INPUTS
+              const SizedBox(height: 27),
+              // MARGIN BETWEEN INPUTS
+              TextInputField(
+                controller: _passController,
+                hint: 'Enter your password',
+                isPass: true,
+                textInputType: TextInputType.text,
+              ),
+              // MARGIN BETWEEN INPUTS AND BUTTON
+              const SizedBox(height: 27),
+              // MARGIN BETWEEN INPUTS AND BUTTON
+              //Button of login
+              InkWell(
+                child: Container(
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                    color: pinkColor,
+                  ),
+                ),
+              ),
+              // MARGIN BETWEEN INPUTS AND BUTTON
+              const SizedBox(height: 27),
+              // MARGIN BETWEEN INPUTS AND BUTTON
+              Flexible(child: Container(), flex: 2),
 
-          //Buttons of login and register
-
-          //Button of forgot password
-        ],
+              //Sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: const Text(
+                        "Sign up.",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-    )));
+    );
   }
 }
---
+++ lib/widgets/text_input_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:identity_project/utils/colors.dart';
+
+class TextInputField extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPass;
+  final String hint;
+  final TextInputType textInputType;
+
+  const TextInputField(
+      {Key? key,
+      required this.controller,
+      this.isPass = false,
+      required this.hint,
+      required this.textInputType})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final inputBorder =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return TextField(
+      controller: controller,
+      textAlign: TextAlign.center,
+      decoration: InputDecoration(
+          hintText: hint,
+          border: inputBorder,
+          focusedBorder: inputBorder,
+          enabledBorder: inputBorder,
+          filled: true,
+          contentPadding: const EdgeInsets.all(8),
+          fillColor: primaryColor), // InputDecoration
+      keyboardType: textInputType,
+      obscureText: isPass,
+    ); // TextField
+  }
+}
