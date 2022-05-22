aaf4bd9b1129272c0a731e33bc872d6733ca58a4
Kaloyan Emilov Kirov <kaloalku@gmail.com>
2022-05-22 22:26:22 +0100

    Added BottomNavbar and PageController for Mobile

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:identity_project/models/user.dart' as model;
-import 'package:identity_project/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:identity_project/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -11,17 +10,135 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: _page);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-        body: Center(
-      child: Text(
-        user.username,
-        style: const TextStyle(
-          color: Colors.blue,
-        ),
+      backgroundColor: appYellowColor,
+      body: PageView(
+        children: [
+          Container(
+            color: appYellowColor,
+            child: const Center(
+              child: Text(
+                'Home',
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Container(
+            color: appYellowColor,
+            child: const Center(
+              child: Text(
+                'Search',
+                style: const TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Container(
+            color: appYellowColor,
+            child: const Center(
+              child: Text(
+                'Upload',
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Container(
+            color: appYellowColor,
+            child: const Center(
+              child: Text(
+                'Favorites',
+                style: const TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Container(
+            color: appYellowColor,
+            child: const Center(
+              child: Text(
+                'Profile',
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+        ],
+        //physics: const NeverScrollableScrollPhysics(), // Elimina el scroll entre paginas.
+        controller: pageController,
+        onPageChanged: onPageChanged,
       ),
-    ));
+      bottomNavigationBar: CupertinoTabBar(
+        height: 60,
+        iconSize: 30,
+        backgroundColor: pinkColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? appBlueColor : appBlackColor,
+            ),
+            label: '',
+            backgroundColor: appBlackColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? appBlueColor : appBlackColor,
+            ),
+            label: '',
+            backgroundColor: appBlackColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? appBlueColor : appWhiteColor,
+            ),
+            label: '',
+            backgroundColor: appBlackColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? appBlueColor : appBlackColor,
+            ),
+            label: '',
+            backgroundColor: appBlackColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_rounded,
+              color: _page == 4 ? appBlueColor : appBlackColor,
+            ),
+            label: '',
+            backgroundColor: appBlackColor,
+          ),
+        ],
+        onTap: navigationTapped,
+      ),
+    );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 const mobileBackgroundColor = Color.fromARGB(255, 229, 166, 18);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
-const orangeColor = Color.fromARGB(255, 223, 108, 0);
-const pinkColor = Color.fromARGB(255, 239, 49, 103);
+const darkOrangeColor = Color.fromARGB(255, 151, 74, 2);
+const pinkColor = Color.fromARGB(255, 239, 49, 102);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
+
+const appBlueColor = Color.fromARGB(255, 8, 184, 191);
+const appYellowColor = Color.fromARGB(255, 254, 184, 20);
+const appOrangeColor = Color.fromARGB(255, 223, 108, 0);
+const appBlackColor = Color.fromARGB(255, 22, 22, 22);
+const appWhiteColor = Color.fromARGB(255, 255, 255, 255);
+
 
 //TODO: Revisar y modificar colores para adaptar al diseño del proyecto.
