d9a88968c34d42b959fc6a4c446394b222133355
Kaloyan Emilov Kirov <kaloalku@gmail.com>
2022-05-25 11:52:13 +0100

    Modified color naming

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromARGB(255, 229, 166, 18);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const darkOrangeColor = Color.fromARGB(255, 151, 74, 2);
@@ -10,6 +9,7 @@ const secondaryColor = Colors.grey;
 
 const appBlueColor = Color.fromARGB(255, 8, 184, 191);
 const appYellowColor = Color.fromARGB(255, 254, 184, 20);
+const appDarkYellowColor = Color.fromARGB(255, 229, 166, 18);
 const appOrangeColor = Color.fromARGB(255, 223, 108, 0);
 const appBlackColor = Color.fromARGB(255, 22, 22, 22);
 const appWhiteColor = Color.fromARGB(255, 255, 255, 255);
