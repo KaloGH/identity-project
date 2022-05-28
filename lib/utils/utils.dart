f939dea512ca36f9aac9204bc7b4624f66a77847
Kaloyan Emilov Kirov <kaloalku@gmail.com>
2022-05-28 14:41:37 +0100

    Removed prints in Utils.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -10,8 +10,6 @@ pickImage(ImageSource source) async {
   if (_file != null) {
     return await _file.readAsBytes();
   }
-
-  print('No image selected');
 }
 
 showSnackBar(String content, BuildContext context, bool isError) {
